97553935eaab18b9777b4a2b21cff80ea5710a7b
tom <123@123.com>
2023-10-18 08:06:07 +0800

    修改了exePy.sh的第三行

--- exePy.sh
+++ exePy.sh
@@ -1,11 +1,12 @@
 pythonPath=`where python | grep "Scripts"`
 echo "python执行路径:"${pythonPath}
-pythonFiles=`find ./ -name *.py`
+pythonFiles=`find ./ -name "*.py"`
+echo "查找到的python文件:"${pythonFiles[*]}
 for file in ${pythonFiles[*]}; 
 do
-	echo "文件名:"${file}
+	#echo ${file}
 	compeltePath=$(pwd)${file:1}
-	echo "文件完整路径:"${compeltePath}
+	echo "当前文件完整路径:"${compeltePath}
 	$pythonPath ${file}
 done
 
